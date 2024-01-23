2e5fabe80f452f60c952ddc65560b0a79c30f75a
Ernesto <hkarakaki@gmail.com>
2024-01-24 01:37:25 +0200

    Added background music, added volume controls to the options menu, save and cancel buttons

--- the-eighth-consciousness/Assets/Game/MainMenu.cs
+++ the-eighth-consciousness/Assets/Game/MainMenu.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -9,9 +10,16 @@ public class MainMenu : MonoBehaviour
     public GUISkin guiSkin;
 
     public Canvas canvas;
+    public AudioMixer audioMixer;
 
     public AudioSource startFX;
     public AudioSource navigateMenuFX;
+    public AudioSource BGMusic;
+
+    private float initialFXVolume;
+    private float initialMusicVolume;
+    private float currentFXVolume;
+    private float currentMusicVolume;
 
     public bool showStartMenu = false;
     public bool ShowStartMenu
@@ -28,21 +36,37 @@ public class MainMenu : MonoBehaviour
     }
 
     Rect windowRect = new Rect(0, 0, 500, 360);
-    bool toggleTxt = false;
-    string stringToEdit = "Text Label";
-    string textToEdit = "TextBox:\nHello World\nI've got few lines...";
-    float hSliderValue = 0.0f;
-    float vSliderValue = 0.0f;
-    float hSbarValue;
-    float vSbarValue;
-    Vector2 scrollPosition = Vector2.zero;
+    float sfxSliderValue;
+    float musicSliderValue;
+
+    public void LoadPlayerPrefs()
+    {
+        initialFXVolume = PlayerPrefs.HasKey("sfxVolume") ? PlayerPrefs.GetFloat("sfxVolume") : 1.0f;
+        initialMusicVolume = PlayerPrefs.HasKey("musicVolume") ? PlayerPrefs.GetFloat("musicVolume") : 1.0f;
+        currentFXVolume = initialFXVolume;
+        currentMusicVolume = initialMusicVolume;
+        sfxSliderValue = initialFXVolume;
+        musicSliderValue = initialMusicVolume;
+    }
 
     void Start()
     {
         windowRect.x = (Screen.width - windowRect.width) / 2;
         windowRect.y = (Screen.height - windowRect.height) / 2;
+
+        LoadPlayerPrefs();
+        audioMixer.SetFloat("sfxVolume", Mathf.Log(currentFXVolume) * 20);
+        audioMixer.SetFloat("musicVolume", Mathf.Log(currentMusicVolume) * 20);
     }
 
+    void Update()
+    {
+        /* The formula to scale the volume was taken from Unity Forum:
+           https://forum.unity.com/threads/changing-audio-mixer-group-volume-with-ui-slider.297884/#post-3494983
+        */
+        audioMixer.SetFloat("sfxVolume", Mathf.Log(currentFXVolume) * 20);
+        audioMixer.SetFloat("musicVolume", Mathf.Log(currentMusicVolume) * 20);
+    }
 
     void OnGUI()
     {
@@ -64,6 +88,7 @@ public class MainMenu : MonoBehaviour
     public void startGame()
     {
         Debug.Log("Start new Game");
+        BGMusic.Stop();
         startFX.Play();
         // Add Animation.
         StartCoroutine(loadScene());
@@ -88,35 +113,85 @@ public class MainMenu : MonoBehaviour
         }
     }
 
+    private void saveSettings()
+    {
+        PlayerPrefs.SetFloat("sfxVolume", currentFXVolume);
+        PlayerPrefs.SetFloat("musicVolume", currentMusicVolume);
+    }
+
+    private void cancelChanges()
+    {
+        currentFXVolume = initialFXVolume;
+        currentMusicVolume = initialMusicVolume;
+        if (!PlayerPrefs.HasKey("sfxVolume"))
+        {
+            PlayerPrefs.SetFloat("sfxVolume", initialFXVolume);
+        }
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", initialMusicVolume);
+        }
+        audioMixer.SetFloat("sfxVolume", Mathf.Log(initialFXVolume) * 20);
+        audioMixer.SetFloat("musicVolume", Mathf.Log(initialMusicVolume) * 20);
+    }
+
     public void optionsMenu(int windowID)
     {
-        GUI.Box(new Rect(10, 50, 120, 250), "Options");
-        GUI.Button(new Rect(20, 80, 100, 20), "BUTTON");
-        GUI.Label(new Rect(20, 115, 100, 20), "LABEL: Hello!");
-        stringToEdit = GUI.TextField(new Rect(15, 140, 110, 20), stringToEdit, 25);
-        hSliderValue = GUI.HorizontalSlider(new Rect(15, 175, 110, 30), hSliderValue, 0.0f, 10.0f);
 
-        vSliderValue = GUI.VerticalSlider(new Rect(140, 50, 20, 200), vSliderValue, 100.0f, 0.0f);
+        GUI.Box(new Rect(10, 40, 480, 380), "Options");
+        // SFX Volume.
+        currentFXVolume = GUI.HorizontalSlider(new Rect(150, 70, 50, 30), currentFXVolume, 0.0001F, 1.0F);
+        if (GUI.Button(new Rect(230, 70, 100, 25), "SFX Test"))
+        {
+            navigateMenuFX.Play();
+        }
+        // Music Volume.
+        currentMusicVolume = GUI.HorizontalSlider(new Rect(150, 100, 50, 30), currentMusicVolume, 0.0001F, 1.0F);
+
+        if (GUI.Button(new Rect(40, 300, 100, 25), "Cancel"))
+        {
+            Debug.Log("Back");
+            navigateMenuFX.Play();
+            showOptionsMenu = false;
+            canvas.enabled = true;
+            cancelChanges();
+        }
+        if (GUI.Button(new Rect(360, 300, 100, 25), "Done"))
+        {
+            Debug.Log("Save and back");
+            navigateMenuFX.Play();
+            showOptionsMenu = false;
+            canvas.enabled = true;
+            saveSettings();
+        }
+
+        /*   GUI.Box(new Rect(10, 50, 120, 250), "Options");
+           GUI.Button(new Rect(20, 80, 100, 20), "BUTTON");
+           GUI.Label(new Rect(20, 115, 100, 20), "LABEL: Hello!");
+           stringToEdit = GUI.TextField(new Rect(15, 140, 110, 20), stringToEdit, 25);
+           hSliderValue = GUI.HorizontalSlider(new Rect(15, 175, 110, 30), hSliderValue, 0.0f, 10.0f);
+
+           vSliderValue = GUI.VerticalSlider(new Rect(140, 50, 20, 200), vSliderValue, 100.0f, 0.0f);
 
 
-        toggleTxt = GUI.Toggle(new Rect(165, 50, 100, 30), toggleTxt, "A Toggle text");
-        textToEdit = GUI.TextArea(new Rect(165, 90, 185, 100), textToEdit, 200);
+           toggleTxt = GUI.Toggle(new Rect(165, 50, 100, 30), toggleTxt, "A Toggle text");
+           textToEdit = GUI.TextArea(new Rect(165, 90, 185, 100), textToEdit, 200);
 
-        GUI.Label(new Rect(180, 215, 100, 20), "ScrollView");
-        scrollPosition = GUI.BeginScrollView(new Rect(180, 235, 160, 100), scrollPosition, new Rect(0, 0, 220, 200));
-        GUI.Button(new Rect(0, 10, 100, 20), "Top-left");
-        GUI.Button(new Rect(120, 10, 100, 20), "Top-right");
-        GUI.Button(new Rect(0, 170, 100, 20), "Bottom-left");
-        GUI.Button(new Rect(120, 170, 100, 20), "Bottom-right");
-        GUI.EndScrollView();
-
-
-        hSbarValue = GUI.HorizontalScrollbar(new Rect(10, 360, 360, 30), hSbarValue, 5.0f, 0.0f, 10.0f);
-        vSbarValue = GUI.VerticalScrollbar(new Rect(380, 25, 30, 300), vSbarValue, 1.0f, 30.0f, 0.0f);
+           GUI.Label(new Rect(180, 215, 100, 20), "ScrollView");
+           scrollPosition = GUI.BeginScrollView(new Rect(180, 235, 160, 100), scrollPosition, new Rect(0, 0, 220, 200));
+           GUI.Button(new Rect(0, 10, 100, 20), "Top-left");
+           GUI.Button(new Rect(120, 10, 100, 20), "Top-right");
+           GUI.Button(new Rect(0, 170, 100, 20), "Bottom-left");
+           GUI.Button(new Rect(120, 170, 100, 20), "Bottom-right");
+           GUI.EndScrollView();
 
 
-        GUI.DragWindow(new Rect(0, 0, 10000, 10000));
-        
+           hSbarValue = GUI.HorizontalScrollbar(new Rect(10, 360, 360, 30), hSbarValue, 5.0f, 0.0f, 10.0f);
+           vSbarValue = GUI.VerticalScrollbar(new Rect(380, 25, 30, 300), vSbarValue, 1.0f, 30.0f, 0.0f);
+
+
+           GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+           */
     }
 
 }
