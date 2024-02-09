d9dd601fde80b7908ff2d38e2ddf7c6325b77713
Ernesto <hkarakaki@gmail.com>
2024-02-09 10:08:40 +0200

    Added more logic to the tutorial scene

--- the-eighth-consciousness/Assets/Game/MainMenu.cs
+++ the-eighth-consciousness/Assets/Game/MainMenu.cs
@@ -218,33 +218,6 @@ public class MainMenu : MonoBehaviour
             saveSettings();
         }
 
-        /*   GUI.Box(new Rect(10, 50, 120, 250), "Options");
-           GUI.Button(new Rect(20, 80, 100, 20), "BUTTON");
-           GUI.Label(new Rect(20, 115, 100, 20), "LABEL: Hello!");
-           stringToEdit = GUI.TextField(new Rect(15, 140, 110, 20), stringToEdit, 25);
-           hSliderValue = GUI.HorizontalSlider(new Rect(15, 175, 110, 30), hSliderValue, 0.0f, 10.0f);
-
-           vSliderValue = GUI.VerticalSlider(new Rect(140, 50, 20, 200), vSliderValue, 100.0f, 0.0f);
-
-
-           toggleTxt = GUI.Toggle(new Rect(165, 50, 100, 30), toggleTxt, "A Toggle text");
-           textToEdit = GUI.TextArea(new Rect(165, 90, 185, 100), textToEdit, 200);
-
-           GUI.Label(new Rect(180, 215, 100, 20), "ScrollView");
-           scrollPosition = GUI.BeginScrollView(new Rect(180, 235, 160, 100), scrollPosition, new Rect(0, 0, 220, 200));
-           GUI.Button(new Rect(0, 10, 100, 20), "Top-left");
-           GUI.Button(new Rect(120, 10, 100, 20), "Top-right");
-           GUI.Button(new Rect(0, 170, 100, 20), "Bottom-left");
-           GUI.Button(new Rect(120, 170, 100, 20), "Bottom-right");
-           GUI.EndScrollView();
-
-
-           hSbarValue = GUI.HorizontalScrollbar(new Rect(10, 360, 360, 30), hSbarValue, 5.0f, 0.0f, 10.0f);
-           vSbarValue = GUI.VerticalScrollbar(new Rect(380, 25, 30, 300), vSbarValue, 1.0f, 30.0f, 0.0f);
-
-
-           GUI.DragWindow(new Rect(0, 0, 10000, 10000));
-           */
     }
 
     public void controlsMenu(int windowID)
--- the-eighth-consciousness/Assets/Game/TutorialController.cs
+++ the-eighth-consciousness/Assets/Game/TutorialController.cs
@@ -7,12 +7,22 @@ using UnityEngine.SceneManagement;
 
 public class TutorialController : MonoBehaviour
 {
+    public GUISkin guiSkin;
+    private Rect windowRect;
+
+    public AudioSource startFX;
+    public AudioSource okFX;
+    public AudioSource BGMusic;
+
     public PlayerStats statsPlayer1;
     public PlayerStats statsPlayer2;
     public GameObject playerPrefab1;
     public GameObject playerPrefab2;
     public Vector3 InitialPosition1;
     public Vector3 InitialPosition2;
+    private GameObject player1;
+    private GameObject player2;
+    private bool hasPlayer2;
 
     private bool hasMoved1;
     private bool hasMoved2;
@@ -25,6 +35,7 @@ public class TutorialController : MonoBehaviour
 
     public void Start()
     {
+        hasPlayer2 = statsPlayer2.IsActive;
         hasMoved1 = !statsPlayer1.IsActive;
         hasFired1 = !statsPlayer1.IsActive;
         hasBombed1 = !statsPlayer1.IsActive;
@@ -36,14 +47,17 @@ public class TutorialController : MonoBehaviour
 
         if (statsPlayer1.IsActive)
         {
-            Instantiate(playerPrefab1, InitialPosition1, Quaternion.identity);
+            player1 = Instantiate(playerPrefab1, InitialPosition1, Quaternion.identity) as GameObject;
             statsPlayer1.UpdateIsAlive(true);
         }
         if (statsPlayer2.IsActive)
         {
-            Instantiate(playerPrefab2, InitialPosition2, Quaternion.identity);
+            player2 = Instantiate(playerPrefab2, InitialPosition2, Quaternion.identity) as GameObject;
             statsPlayer2.UpdateIsAlive(true);
         }
+
+        windowRect = new Rect(0, 0, 500, 360);
+
     }
 
     void Update()
@@ -60,7 +74,13 @@ public class TutorialController : MonoBehaviour
             )
         {
             endTutorial();
-        }        
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin = guiSkin;
+        windowRect = GUI.Window(0, windowRect, movementHelp, "Controls");
     }
 
     private void endTutorial()
@@ -74,4 +94,88 @@ public class TutorialController : MonoBehaviour
         SceneManager.LoadScene("Proto1");
     }
 
+
+    private void movementHelp(int windowID)
+    {
+        if (hasPlayer2)
+        {
+            GUI.Box(new Rect(10, 40, 240, 370), "Player 2");
+            GUI.Label(new Rect(270, 220, 60, 20), "Move");
+            GUI.enabled = false;
+            GUI.Button(new Rect(340, 90, 40, 20), "\u21E7");
+            GUI.Button(new Rect(290, 130, 40, 20), "\u21E6");
+            GUI.Button(new Rect(340, 130, 40, 20), "\u21E9");
+            GUI.Button(new Rect(390, 130, 40, 20), "\u21E8");
+            GUI.enabled = true;
+            return;
+        }
+        GUI.Box(new Rect(10, 40, 240, 370), "Player 1");
+        GUI.Label(new Rect(30, 220, 60, 20), "Move");
+        GUI.enabled = false;
+        GUI.Button(new Rect(100, 90, 40, 20), "W");
+        GUI.Button(new Rect(50, 130, 40, 20), "A");
+        GUI.Button(new Rect(100, 130, 40, 20), "S");
+        GUI.Button(new Rect(150, 130, 40, 20), "D");
+        GUI.enabled = true;
+        return;
+
+    }
+
+    private void fireHelp(int windowID)
+    {
+        if (hasPlayer2)
+        {
+            GUI.Box(new Rect(10, 40, 240, 370), "Player 2");
+            GUI.Label(new Rect(270, 220, 60, 20), "Shoot");
+            GUI.enabled = false;
+            GUI.Button(new Rect(340, 220, 40, 20), "1");
+            GUI.enabled = true;
+            return;
+        }
+
+        GUI.Box(new Rect(10, 40, 240, 370), "Player 1");
+        GUI.Label(new Rect(30, 220, 60, 20), "Shoot");
+        GUI.enabled = false;
+        GUI.Button(new Rect(100, 220, 40, 20), "Y");
+        GUI.enabled = true;
+        return;
+    }
+
+    private void bombHelp(int windowID)
+    {
+        if (hasPlayer2)
+        {
+            GUI.Box(new Rect(10, 40, 240, 370), "Player 2");
+            GUI.Label(new Rect(270, 220, 60, 20), "Bomb");
+            GUI.enabled = false;
+            GUI.Button(new Rect(340, 220, 40, 20), "2");
+            GUI.enabled = true;
+            return;
+        }
+        GUI.Box(new Rect(10, 40, 240, 370), "Player 1");
+        GUI.Label(new Rect(30, 220, 60, 20), "Bomb");
+        GUI.enabled = false;
+        GUI.Button(new Rect(100, 220, 40, 20), "U");
+        GUI.enabled = true;
+        return;
+    }
+
+    private void ecdHelp(int windowID)
+    {
+        if (hasPlayer2)
+        {
+            GUI.Box(new Rect(10, 40, 240, 370), "Player 2");
+            GUI.Label(new Rect(30, 260, 60, 20), "ECD");
+            GUI.enabled = false;;
+            GUI.Button(new Rect(340, 260, 40, 20), "3");
+            GUI.enabled = true;
+            return;
+        }
+        GUI.Box(new Rect(10, 40, 240, 370), "Player 1");
+        GUI.Label(new Rect(30, 260, 60, 20), "ECD");
+        GUI.enabled = false;
+        GUI.Button(new Rect(100, 260, 40, 20), "I");
+        GUI.enabled = true;
+        return;
+    }
 }
